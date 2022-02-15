9fd10f6b6d74c4d13777dcf94f6eb52a50aa4f67
Luca S <lucasky84@gmail.com>
2022-02-15 11:26:46 +0100

    Create odoo-install.sh

---
+++ odoo-install.sh
@@ -0,0 +1,7 @@
+apt-get update
+apt-get upgrade -y
+apt-get install postgresql -y
+wget -O - https://nightly.odoo.com/odoo.key | apt-key add -
+echo "deb http://nightly.odoo.com/15.0/nightly/deb/ ./" >> /etc/apt/sources.list.d/odoo.list
+apt-get update
+apt-get install odoo -y
