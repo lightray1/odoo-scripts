56d8656de3e19e87c89a1098e98ff94684c9423d
Luca S <lucasky84@gmail.com>
2022-02-15 17:42:31 +0100

    Update odoo-install.sh

--- odoo-install.sh
+++ odoo-install.sh
@@ -4,4 +4,4 @@ apt-get install postgresql -y
 wget -O - https://nightly.odoo.com/odoo.key | apt-key add -
 echo "deb http://nightly.odoo.com/15.0/nightly/deb/ ./" >> /etc/apt/sources.list.d/odoo.list
 apt-get update
-apt-get install odoo -y
+apt-get install odoo nginx certbot python3-pip -y
